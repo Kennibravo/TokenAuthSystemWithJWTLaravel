e66c55c4da2aad7ee84c38b0ee6173ea5cda2044
tripletens <tripletens.kc@gmail.com>
2019-02-22 16:12:13 +0100

    artisan addition to the jwt auth
    just added the table and timestamp = true

--- app/Artisan.php
+++ app/Artisan.php
@@ -2,50 +2,18 @@
 
 namespace App;
 
-
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
-
-
-class Artisan extends Authenticatable implements JWTSubject
-{
-
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    public function getJWTIdentifier()
-        {
-            return $this->getKey();
-        }
-        
-        public function getJWTCustomClaims()
-        {
-            return [];
-        }
-=======
 use Illuminate\Database\Eloquent\Model;
 
 class Artisan extends Model
 {
     //
->>>>>>> d7326188948a83cbc56b24d460d3980a62a3722d
+    /// //Tble name
+    protected $table = 'artisans';
+    //Primary key
+    public $primarykey = 'id';
+    //timestamps
+    public $timestamps = true;
+
+    protected $fillable = ['title', 'body'];
+
 }
